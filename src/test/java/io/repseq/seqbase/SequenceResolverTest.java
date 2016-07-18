9e4bee95656b50cad63003e45c5de18909803978
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2016-07-18 13:04:23 +0300

    Fixes for tests.

--- src/test/java/io/repseq/seqbase/SequenceResolverTest.java
+++ src/test/java/io/repseq/seqbase/SequenceResolverTest.java
@@ -4,26 +4,44 @@ import com.milaboratory.core.Range;
 import com.milaboratory.core.io.sequence.fastq.SingleFastqReaderTest;
 import com.milaboratory.core.sequence.NucleotideSequence;
 import com.milaboratory.util.TempFileManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class SequenceResolverTest {
     @Test
     public void test1() throws Exception {
-        SequenceResolvers.initDefaultResolver(TempFileManager.getTempDir().toPath());
+        Path dir = TempFileManager.getTempDir().toPath().toAbsolutePath();
+
+        Path work = dir.resolve("work");
+        Files.createDirectories(work);
+
+        Path cache = dir.resolve("cache");
+        Files.createDirectories(cache);
+
+        SequenceResolvers.initDefaultResolver(cache);
         SequenceResolver defaultResolver = SequenceResolvers.getDefault();
 
         NucleotideSequence seq = defaultResolver.resolve(new SequenceAddress("nuccore://EU877942.1"))
                 .getRegion(new Range(10, 30).inverse());
         Assert.assertEquals(new NucleotideSequence("gcgagagcaagcactatggc").getReverseComplement(), seq);
 
-        Path rootPath = new File(SingleFastqReaderTest.class.getClassLoader().getResource("sequences/some_fasta.fasta")
-                .toURI()).toPath().toAbsolutePath().normalize().getParent();
+        Path someFasta = work.resolve("some_fasta.fasta");
+        FileUtils.copyToFile(
+                SingleFastqReaderTest.class.getClassLoader().getResourceAsStream("sequences/some_fasta.fasta"),
+                someFasta.toFile());
 
-        seq = defaultResolver.resolve(new SequenceAddress(rootPath, "file://some_fasta.fasta#24.6jsd21.Tut")).getRegion(new Range(10, 30));
+        //Path rootPath = new File(SingleFastqReaderTest.class.getClassLoader().getResource("sequences/some_fasta.fasta")
+        //        .toURI()).toPath().toAbsolutePath().normalize().getParent();
+
+        seq = defaultResolver.resolve(
+                new SequenceAddress(work,
+                        "file://some_fasta.fasta#24.6jsd21.Tut"))
+                .getRegion(new Range(10, 30));
+
         Assert.assertEquals(new NucleotideSequence("ATCCTGGCTTAGAACTAACG"), seq);
     }
 }
