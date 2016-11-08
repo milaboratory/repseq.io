342b0f8b5ce01ce395ca74433476ab5323dc7fd1
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2016-11-08 14:31:56 +0300

    repseq built-in library updated to v1.1 . Added code to incorporate previous library build for backward compatibility.

--- src/main/java/io/repseq/maven/CompileLibraryMavenStage.java
+++ src/main/java/io/repseq/maven/CompileLibraryMavenStage.java
@@ -30,18 +30,37 @@ public class CompileLibraryMavenStage {
     public static void main(String[] args) throws IOException, InterruptedException {
         Path root = Paths.get(args[0]);
 
-        Path cache = root.resolve(".cache");
-        SequenceResolvers.initDefaultResolver(cache);
-
-        AtomicInteger counter = new AtomicInteger();
-        List<Path> compiledPaths = new ArrayList<>();
-
-        Path target = root.resolve("target").resolve("library");
-        Files.createDirectories(target);
-
+        Path cacheFolder = root.resolve(".cache");
+        Path buildFolder = root.resolve("target").resolve("library");
+        Path outputFolder = root.resolve("target").resolve("classes").resolve("libraries");
         Path libraryRepoFolder = root.resolve("library");
 
-        compileDir(target, counter, compiledPaths, libraryRepoFolder);
+        Process gitTagProcess = new ProcessBuilder("git", "describe", "--always", "--tags")
+                .directory(libraryRepoFolder.toFile())
+                .start();
+        String currentTag = IOUtils.toString(gitTagProcess.getInputStream(), StandardCharsets.UTF_8)
+                .replace("\n", "").replace("\r", "");
+        gitTagProcess.waitFor();
+
+        String[] targetTags = {"v1.0", currentTag};
+
+        for (String tag : targetTags) {
+            new ProcessBuilder("git", "checkout", tag)
+                    .directory(libraryRepoFolder.toFile())
+                    .inheritIO()
+                    .start()
+                    .waitFor();
+            process(libraryRepoFolder, cacheFolder, buildFolder, outputFolder, tag, tag.equals(currentTag));
+        }
+    }
+
+    public static void process(Path libraryRepoFolder, Path cacheFolder, Path buildFolder, Path outputFolder,
+                               String tag, boolean isDefault) throws IOException, InterruptedException {
+        SequenceResolvers.initDefaultResolver(cacheFolder);
+
+        Files.createDirectories(buildFolder);
+
+        List<Path> compiledPaths = compileDir(libraryRepoFolder, buildFolder);
 
         List<VDJCLibraryData> libs = new ArrayList<>();
 
@@ -53,39 +72,40 @@ public class CompileLibraryMavenStage {
 
         log.info("Merged successfully.");
 
-        Path libResourcePath = root.resolve("target").resolve("classes").resolve("libraries");
-        Files.createDirectories(libResourcePath);
+        Files.createDirectories(outputFolder);
 
-        Process gitTagProcess = new ProcessBuilder("git", "describe", "--always", "--tags")
-                .directory(libraryRepoFolder.toFile())
-                .start();
-        String gitTag = IOUtils.toString(gitTagProcess.getInputStream(), StandardCharsets.UTF_8)
-                .replace("\n", "").replace("\r", "");
-        gitTagProcess.waitFor();
-
-        String fullLibraryName = "repseqio." + gitTag;
-        Path resultPath = libResourcePath.resolve(fullLibraryName + ".json");
+        String fullLibraryName = "repseqio." + tag;
+        Path resultPath = outputFolder.resolve(fullLibraryName + ".json");
 
         log.info("Writing {}", resultPath);
 
         VDJCDataUtils.writeToFile(mergeResult, resultPath, true);
 
-        Path aliasPath = libResourcePath.resolve("default.alias");
+        if (isDefault) {
+            Path aliasPath = outputFolder.resolve("default.alias");
 
-        log.info("Writing {}", aliasPath);
+            log.info("Writing {}", aliasPath);
 
-        try (FileOutputStream fos = new FileOutputStream(aliasPath.toFile())) {
-            fos.write(fullLibraryName.getBytes(StandardCharsets.UTF_8));
+            try (FileOutputStream fos = new FileOutputStream(aliasPath.toFile())) {
+                fos.write(fullLibraryName.getBytes(StandardCharsets.UTF_8));
+            }
         }
     }
 
-    public static void compileDir(Path to, AtomicInteger counter, List<Path> compiledPaths, Path parent) throws IOException {
+    public static List<Path> compileDir(Path libraryRepoFolder, Path to) throws IOException {
+        AtomicInteger counter = new AtomicInteger();
+        List<Path> resultFiles = new ArrayList<>();
+        compileDir(to, counter, resultFiles, libraryRepoFolder);
+        return resultFiles;
+    }
+
+    public static void compileDir(Path to, AtomicInteger counter, List<Path> resultFiles, Path parent) throws IOException {
         for (Path path : Files.newDirectoryStream(parent)) {
             if (Files.isDirectory(path))
-                compileDir(to, counter, compiledPaths, path);
+                compileDir(to, counter, resultFiles, path);
             else if (path.getFileName().toString().endsWith(".json")) {
                 Path resultPath = to.resolve("lib" + counter.incrementAndGet() + ".json");
-                compiledPaths.add(resultPath);
+                resultFiles.add(resultPath);
                 CompileAction.compile(path, resultPath, 30);
             }
         }
